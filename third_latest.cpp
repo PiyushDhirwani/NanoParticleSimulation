d323c9828380e5f83aeb18eac540800f0edb8a5e
PiyushDhirwani <piyush.dhirwani@iitgn.ac.in>
2023-09-18 11:16:10 +0530

    trucantion added

--- third_latest.cpp
+++ third_latest.cpp
@@ -24,6 +24,56 @@ vector<float> FindCordinate2D(float EdgeLength, float Angle)
     return Cordinate;
 }
 
+void Input2D(int NumberOfParitcles, ofstream &outputFile)
+{
+    float ParticleType1, ParticleSize1;
+    cout << "Enter ParticleType and ParticleSize For Particle 1: ";
+    cin >> ParticleType1 >> ParticleSize1;
+    vector<float> Cordinate(2, 0);
+    outputFile << "ParticleID x y ParticleType ParticleSize" << endl;
+    outputFile << 1 << "          " << Cordinate[0] << " " << Cordinate[1] << " " << ParticleType1 << "            " << ParticleSize1 << endl;
+    for (int i = 1; i < NumberOfParitcles; i++)
+    {
+        float EdgeLength, Angle, ParticleType, ParticleSize;
+        cout << "Enter Distance with Respect to Particle 1 as Reference Point, Angle, ParticleType, ParticleSize For Particle " << i + 1 << ": ";
+        cin >> EdgeLength >> Angle >> ParticleType >> ParticleSize;
+        Cordinate = FindCordinate2D(EdgeLength, Angle);
+        for (int iterator = 0; iterator < 2; iterator++){
+            if ((Cordinate[iterator] > -1e-5 and Cordinate[iterator] < 0) or (Cordinate[iterator] < 1e-5 and Cordinate[iterator] > 0))
+            {
+                Cordinate[iterator] = 0;
+            }
+        }
+        outputFile << i + 1 << "          " << Cordinate[0] << " " << Cordinate[1] << " " << ParticleType << "            " << ParticleSize << endl;
+    }
+    return;
+}
+
+void Input3D(int NumberOfParitcles, ofstream &outputFile)
+{
+    float ParticleType1, ParticleSize1;
+    cout << "Enter ParticleType and ParticleSize For Particle 1: ";
+    cin >> ParticleType1 >> ParticleSize1;
+    vector<float> Cordinate(3, 0);
+    outputFile << "ParticleID x y z ParticleType ParticleSize" << endl;
+    outputFile << 1 << "          " << Cordinate[0] << " " << Cordinate[1] << " " << Cordinate[2] << " " << ParticleType1 << "            " << ParticleSize1 << endl;
+    for (int i = 1; i < NumberOfParitcles; i++)
+    {
+        float EdgeLength, AnglePhi, AngleTheta, ParticleType, ParticleSize;
+        cout << "Enter Distance with Respect to Particle 1 as Reference Point, AnglePhi, AngleTheta, ParticleType, ParticleSize For Particle " << i + 1 << ": ";
+        cin >> EdgeLength >> AnglePhi >> AngleTheta >> ParticleType >> ParticleSize;
+        Cordinate = FindCordinate3D(EdgeLength, AnglePhi, AngleTheta);
+        for (int iterator = 0; iterator < 3; iterator++){
+            if ((Cordinate[iterator] > -1e-5 and Cordinate[iterator] < 0) or (Cordinate[iterator] < 1e-5 and Cordinate[iterator] > 0))
+            {
+                Cordinate[iterator] = 0;
+            }
+        }
+        outputFile << i + 1 << "          " << Cordinate[0] << " " << Cordinate[1] << " " << Cordinate[2] << " " << ParticleType << "            " << ParticleSize << endl;
+    }
+    return;
+}
+
 int main(int argc, char const *argv[])
 {
     // vector<double> Cordinate = FindCordinate3D(3, 60, 45);
@@ -42,37 +92,11 @@ int main(int argc, char const *argv[])
     cin >> StructureType;
     if (StructureType == 0 or NumberOfParitcles <= 3)
     {
-        int ParticleType1, ParticleSize1;
-        cout << "Enter ParticleType and ParticleSize For Particle 1: ";
-        cin >> ParticleType1 >> ParticleSize1;
-        vector<float> Cordinate = {0, 0};
-        outputFile << "ParticleID x y ParticleType ParticleSize" << endl;
-        outputFile << 1 << " " << Cordinate[0] << " " << Cordinate[1] << " " << ParticleType1 << " " << ParticleSize1 << endl;
-        for (int i = 1; i < NumberOfParitcles; i++)
-        {
-            float EdgeLength, Angle, ParticleType, ParticleSize;
-            cout << "Enter Distance with Respect to Particle 1 as Reference Point, Angle, ParticleType, ParticleSize For Particle " << i + 1 << ": ";
-            cin >> EdgeLength >> Angle >> ParticleType >> ParticleSize;
-            Cordinate = FindCordinate2D(EdgeLength, Angle);
-            outputFile << i + 1 << " " << Cordinate[0] << " " << Cordinate[1] << " " << ParticleType << " " << ParticleSize << endl;
-        }
+        Input2D(NumberOfParitcles, outputFile);
     }
     else
     {
-        int ParticleType1, ParticleSize1;
-        cout << "Enter ParticleType and ParticleSize For Particle 1: ";
-        cin >> ParticleType1 >> ParticleSize1;
-        vector<float> Cordinate = {0, 0, 0};
-        outputFile << "ParticleID x y z ParticleType ParticleSize" << endl;
-        outputFile << 1 << " " << Cordinate[0] << " " << Cordinate[1] << " " << Cordinate[2] << " " << ParticleType1 << " " << ParticleSize1 << endl;
-        for (int i = 1; i < NumberOfParitcles; i++)
-        {
-            float EdgeLength, AnglePhi, AngleTheta, ParticleType, ParticleSize;
-            cout << "Enter Distance with Respect to Particle 1 as Reference Point, AnglePhi, AngleTheta, ParticleType, ParticleSize For Particle " << i + 1 << ": ";
-            cin >> EdgeLength >> AnglePhi >> AngleTheta >> ParticleType >> ParticleSize;
-            Cordinate = FindCordinate3D(EdgeLength, AnglePhi, AngleTheta);
-            outputFile << i + 1 << " " << Cordinate[0] << " " << Cordinate[1] << " " << Cordinate[2] << " " << ParticleType << " " << ParticleSize << endl;
-        }
+        Input3D(NumberOfParitcles, outputFile);
     }
     outputFile.close();
     return 0;
